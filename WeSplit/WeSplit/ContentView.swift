8040a30d1d37fba12bfd72eb2ef08f83480a3757
fanwar-onshape <fanwar@ptc.com>
2023-01-04 14:43:28 +0530

    Updated the UI with controls to add and calculate totalPerPerson based on number of person, tip percent and total amount.

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,18 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Creating a new property with a special type. For this app, we want it to work with Doubles.
+    var dollarFormat : FloatingPointFormatStyle<Double>.Currency {
+       // Magic spell here
+       // not sure what goes here, but we KNOW we need to return a FloatingPointFormatStyle.
+        if #available(iOS 16, *) {
+            return FloatingPointFormatStyle<Double>.Currency(code: Locale.current.currency?.identifier ?? "INR")
+        } else {
+            // Fallback on earlier versions
+            return FloatingPointFormatStyle<Double>.Currency(code: Locale.current.currencyCode ?? "INR")
+        }
+    }
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+
+    let tipPercentages = [10, 15, 20, 25, 0]
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationView {
-            Form{
-                HStack {
-                    Image(systemName: "globe")
-                        .imageScale(.small)
-                        .foregroundColor(.accentColor)
-                    Text("Hello, World!")
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: dollarFormat)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                           ForEach(2 ..< 100) {
+                               Text("\($0) people")
+                           }
+                       }
+                    }
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                Section {
+                        Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    }
+            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
                 }
             }
-            .navigationTitle("Greetings")
-            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
