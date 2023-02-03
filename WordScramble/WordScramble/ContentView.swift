0c81f5615fa8a60c0165c82783d1618f9f86d23f
fanwar-onshape <fanwar@ptc.com>
2023-02-03 12:15:24 +0530

    WordScramble is a game that will show players a random eight-letter word, and ask them to make words out of it. For example, if the starter word is “alarming” they might spell “alarm”, “ring”, “main”, and so on.

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Fauad Anwar on 03/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Fauad Anwar on 03/02/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
