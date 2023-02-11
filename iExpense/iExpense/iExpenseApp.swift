fdebf84c963567ce1041d2eca7b9f2500c3291f3
fanwar-onshape <fanwar@ptc.com>
2023-02-11 20:10:42 +0530

    Adding iExpense app is an expense tracker that separates personal costs from business costs.

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Fauad Anwar on 11/02/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
