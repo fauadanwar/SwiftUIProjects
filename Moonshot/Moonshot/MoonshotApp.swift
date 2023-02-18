5da5e97379c9d368281ffd647ef84e771a1941ee
fanwar-onshape <fanwar@ptc.com>
2023-02-18 16:14:28 +0530

    Moonshot is an app that lets users learn about the missions and astronauts that formed NASA’s Apollo space program.

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Fauad Anwar on 18/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
