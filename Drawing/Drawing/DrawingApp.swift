18821f9fdd62abf6505c32ed0f461609f6790ccf
fanwar-onshape <fanwar@ptc.com>
2023-03-04 20:23:30 +0530

    Added Drawing project to learn about paths, shapes, inset table shapes and more.

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Fauad Anwar on 04/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
