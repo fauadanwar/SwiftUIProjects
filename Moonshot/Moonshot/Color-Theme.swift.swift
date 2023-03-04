63d38c6855463877edbd115652d49b001d45354e
fanwar-onshape <fanwar@ptc.com>
2023-03-04 20:16:29 +0530

    Added theme colour to Moonshot

---
+++ Moonshot/Moonshot/Color-Theme.swift.swift
@@ -0,0 +1,18 @@
+//
+//  Color-Theme.swift.swift
+//  Moonshot
+//
+//  Created by Fauad Anwar on 01/03/23.
+//
+
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.3)
+    }
+}
